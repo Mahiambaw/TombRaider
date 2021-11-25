b316677dfcf7f9f5c2b89674726035037fe719c0
Noah Lundberg <nhlund@gmail.com>
2021-11-25 14:27:55 +0100

    character changes

--- script/Background.js
+++ script/Background.js
@@ -19,6 +19,7 @@ class Background extends Phaser.Scene {
 
     this.load.tilemapTiledJSON('map', './assets/dungeoun/level1.json')
     this.load.spritesheet('dude', '../assets/sprites/warrior.png', { frameWidth: 69, frameHeight: 44 });
+    this.load.spritesheet('enemy', '../assets/sprites/enemy.png', { frameWidth: 69, frameHeight: 44 });
   }
 
   create() {
@@ -26,9 +27,9 @@ class Background extends Phaser.Scene {
     const layers = this.creatLayer(map);
 
     const playerZone = this.getplayerZone(layers.playerZone)
-    console.log(playerZone.end)
 
     this.player = this.creatPlayer(playerZone);
+    this.enemy = this.creatEnemy()
 
     this.anims.create({
       key: 'idle',
@@ -55,25 +56,72 @@ class Background extends Phaser.Scene {
       frameRate: 20,
       repeat: 0
     });
+    //GLIDE
     this.anims.create({
       key: "glide",
       frames: this.anims.generateFrameNumbers('dude', { start: 85, end: 90 }),
       frameRate: 10,
-      repeat: -1
+      repeat: 0
+    });
+    //FALL
+    this.anims.create({
+      key: "fall",
+      frames: this.anims.generateFrameNumbers('dude', { start: 46, end: 48}),
+      frameRate: 10,
+      repeat: 0
     })
     //----------------------END---------------------
 
 
+    // enemy animation --------------------
+    this.anims.create({
+      key: 'enemy_idle',
+      frames: this.anims.generateFrameNumbers('enemy', { start: 60, end: 68 }),
+      frameRate: 8,
+      repeat: -1
 
-    this.physics.add.collider(this.player, layers.platforms)
+    })
+
+    this.anims.create({
+      key: 'enemy_run',
+      frames: this.anims.generateFrameNumbers('enemy', { start: 0, end: 8 }),
+      FrameRate: 8,
+      repeat: -1
+
+    })
+    this.anims.create({
+      key: 'enemy_Back',
+      frames: this.anims.generateFrameNumbers('enemy', { start: 39, end: 46 }),
+      FrameRate: 8,
+      repeat: -1
+
+    })
+    this.anims.create({
+      key: 'enemy_Front',
+      frames: this.anims.generateFrameNumbers('enemy', { start: 47, end: 54 }),
+      FrameRate: 8,
+      repeat: -1
+
+    })
+    //-------- end-----------------
+    //------- collaider for both enemy ------------
+    this.enemy.setImmovable(true);
+    this.enemy.setSize(this.player.width, this.player.height)
     this.endOfLevel(playerZone.end, this.player)
+    this.physics.add.collider(this.player, layers.platforms)
+    this.physics.add.collider(this.enemy, layers.platforms)
+    this.physics.add.collider(this.enemy, this.player)
+
+    //---------------------- end ---------------------
 
     this.box = Phaser.GameObjects.Rectangle
 
 
-    // this.box = this.add.rectangle(null, null, 30, 30, 0xffffff);
-    // this.add.existing(this.box);
-    // //this.creatCamera()
+    this.box = this.add.rectangle(null, null, 30, 30, 0xffffff);
+    this.add.existing(this.box);
+    this.box.active = false;
+    this.box.alpha = 0.2;
+    //this.creatCamera()
     //this.input.keyboard.on('keydown-UP', pressCheck);
 
 
@@ -104,6 +152,7 @@ class Background extends Phaser.Scene {
 
     const playerZone = map.getObjectLayer('player_Zone').objects;
     platforms.setCollisionByExclusion(-1, true);
+
     return { background, platforms, playerZone }
 
 
@@ -113,7 +162,9 @@ class Background extends Phaser.Scene {
   creatPlayer({ start }) {
 
     let player = this.physics.add.sprite(start.x, start.y, 'dude');
-    console.log(start.x, start.y, "this is start")
+
+    //let player = this.physics.add.sprite(1410, 500, 'dude');
+    //console.log(start.x, start.y, "this is start")
     player.body.setGravityY(700);
     player.setCollideWorldBounds(true);
     player.setBodySize(20, 30);
@@ -122,9 +173,23 @@ class Background extends Phaser.Scene {
     return player;
 
   }
+  creatEnemy() {
 
+    let enemy = this.physics.add.sprite(900, 490, 'enemy');
+
+    //let player = this.physics.add.sprite(1410, 500, 'dude');
+    //console.log(start.x, start.y, "this is start")
+
+    enemy.body.setGravityY(700);
+    enemy.setCollideWorldBounds(true);
+
+    //enemy.body.setOffset(18, 13);
+
+    return enemy;
+
+  }
   cameraFollow(player) {
-    const { height, wdith, mapOffset, zoomFactor } = config;
+    const { height, wdith, zoomFactor } = config;
     //this.physics.world.setBounds(0, 0, wdith + mapOffset, height)
     this.cameras.main.setBounds(0, 0, wdith, height).setZoom(zoomFactor);
     this.cameras.main.startFollow(player);
@@ -139,22 +204,23 @@ class Background extends Phaser.Scene {
     }
   }
   endOfLevel(end, player) {
+
     //console.log(end.x, end.y, "end")
     const endLevel = this.physics.add.sprite(end.x, end.y,
-      'end').setSize(5, 200).setOrigin(0.5, 1)
-    console.log(end.x, end.y, "end1")
-    const overlap = this.physics.add.overlap(this.player, endLevel, () => {
-      overlap.active = false;
-      console.log(end.x, end.y, "end")
+      'end').setAlpha(0)
+    endLevel.body.allowGravity = false;
+    const endlap = this.physics.add.overlap(player, endLevel, () => {
+      endlap.active = false;
+      //console.log(end.x, end.y, "end")
       console.log("player has won ")
     })
+    this.player.anims.play('idle', true);
+
   }
 
 
 
-
   update() {
-
 
     const onFloor = this.player.body.onFloor(); // checks if the player has touched the platform collider 
     const cursors = this.input.keyboard.createCursorKeys();
@@ -162,81 +228,118 @@ class Background extends Phaser.Scene {
     const ctrl = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.CTRL);
     const isUpDown = Phaser.Input.Keyboard.JustDown(cursors.up)
 
+    this.player.on('animationcomplete', () => {animCheck = false});
 
     //----------------PLAY ANIMATIONS------------------
     //HOLDING LEFT
     if (cursors.left.isDown) {
       //SET RUNNING SPEED TO 300 TO LEFT
       this.player.setVelocityX(-300);
-      //SET POSITION OF SPRITE HITBOX TO CENTER OF SPRITE
-      this.player.body.setOffset(30, 13);
       //IF WE ARE TOUCHING THE GROUND AND NOT ATTACKING DO "RUN" ANIMATION
       if (this.player.body.blocked.down && !animCheck) this.player.anims.play('run', true);
       //FLIP SPRITE TO THE LEFT
       this.player.flipX = true;
     }
+
+
     //HOLDING RIGHT, SEE ABOVE
     else if (cursors.right.isDown) {
       this.player.setVelocityX(300);
-      this.player.body.setOffset(18, 13);
       if (this.player.body.blocked.down && !animCheck) this.player.anims.play('run', true);
       this.player.flipX = false;
     }
+
+
     //IF WE ARE NOT DOING ANYTHING DO THE IDLE ANIMATION
     else {
       //SET IT SO CHARACTER STOPS
       this.player.setVelocityX(0);
       if (this.player.body.blocked.down && !animCheck) this.player.anims.play('idle', true);
     }
+
     //IF YOU PRESS DOWN
-    if (isUpDown && (onFloor || this.jumpCount < this.nextJumps)) {
+    if (isUpDown && (onFloor || this.jumpCount < this.nextJumps) && !animCheck) {
       //SETS SPEED GOING DOWN
       this.player.setVelocityY(-600);
       this.jumpCount++
+      if(!animCheck)this.player.anims.play('jump', true);
       console.log(this.jumpCount)
     }
+
+    //FALLING FAST
+    if(this.player.body.velocity.y != 0 && cursors.down.isDown) {
+      this.player.setVelocityY(600);
+    }
+
+    //FALLING
+    if (this.player.body.velocity.y > 0 && this.jumpCount == 0) {
+      if(!animCheck)this.player.anims.play('fall', true);
+    }
+
+    //RESET JUMPCOUNT ON TOUCHING THE FLOOR
     if (onFloor) {
       this.jumpCount = 0;
     }
-    if (ctrl.isDown) {
+
+    //GLIDING
+    if (ctrl.isDown && onFloor && this.player.body.velocity.x != 0) {
       this.player.anims.play('glide', true)
+      animCheck = true;
     }
+ 
+
     //ATTACK ANIMATION IF YOU PRESS DOWN ARROW
     if (space.isDown) {
       this.player.anims.play('slash', true);
       //MAKE HITBOX FOR WEAPON ACTIVE
-      this.box.active = true
+      //this.box.active = true
       //MAKE SURE THE ANIMATION GETS TO RUN
       animCheck = true;
-      this.player.on('animationcomplete', resetNr);
     }
 
+    //ENABLE ATTACK WHEN SLASHING
+    if(this.player.anims.currentAnim.key == "slash") {
+      this.box.active = true;
+    }
+    //DISABLE ATTACKING WHEN NOT SLASHING
+    else {
+      this.box.active = false;
+    }
+
+    if(this.player.anims.currentAnim.key == "glide") {
+      this.player.setBodySize(20, 15);
+      this.player.body.setOffset(18, 28);
+    }
+
+    else {
+      this.player.setBodySize(20, 30);
+      this.player.body.setOffset(18, 13);
+    }
+
+    //IF PLAYER IS TURNED TO THE RIGHT
     if (!this.player.flipX) {
       this.box.x = this.player.x + 20;
       this.box.y = this.player.y + 5;
+      //SET POSITION OF SPRITE HITBOX TO CENTER OF SPRITE
+      if(this.player.anims.currentAnim.key != "glide")this.player.body.setOffset(18, 13);
     }
-
+    //IF PLAYER IS TURNED TO THE LEFT
     if (this.player.flipX) {
       this.box.x = this.player.x - 20;
       this.box.y = this.player.y + 5;
+      //SET POSITION OF SPRITE HITBOX TO CENTER OF SPRITE
+      if(this.player.anims.currentAnim.key != "glide")this.player.body.setOffset(30, 13);
     }
 
+    
 
     if (this.player.body.blocked.down) dbJump = 0;
     //console.log(player.y);
   }
 
 }
-function pressCheck() {
-  //SETS SPEED GOING UP
-  if (dbJump < 2) {
-    this.player.setVelocityY(-600);
-    this.player.anims.play('jump', true);
-    dbJump++;
-  }
-}
 
-//  
+
 function render() {
   game.debug.inputInfo(32, 32);
 }
