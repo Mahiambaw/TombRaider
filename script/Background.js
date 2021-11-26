20dbb3c9c57ffb2857794f6f3149c9f7d1ca214b
TabassomRoohafza <tabassomroohafza@hyperisland.se>
2021-11-26 03:06:34 +0100

    adding menu and enemy

--- script/Background.js
+++ script/Background.js
@@ -1,6 +1,9 @@
 let emitter, particles, animCheck = 0
 let attackBox, dbJump = 0;
 let box;
+let stepLimit = 100;
+let center;
+let clickStart = false;
 
 class Background extends Phaser.Scene {
 
@@ -19,7 +22,11 @@ class Background extends Phaser.Scene {
 
     this.load.tilemapTiledJSON('map', './assets/dungeoun/level1.json')
     this.load.spritesheet('dude', '../assets/sprites/warrior.png', { frameWidth: 69, frameHeight: 44 });
-    this.load.spritesheet('enemy', '../assets/sprites/enemy.png', { frameWidth: 69, frameHeight: 44 });
+    this.load.spritesheet('enemy', '../assets/sprites/enemy.png', { frameWidth: 41.4, frameHeight: 41.4 });
+    
+    this.load.image('button', '../assets/btn/a.png')
+    this.load.image('helpBox', '../assets/btn/help.png')
+    this.load.image('closeHelpBox', '../assets/btn/close.png')
   }
 
   create() {
@@ -30,7 +37,8 @@ class Background extends Phaser.Scene {
 
     this.player = this.creatPlayer(playerZone);
     this.enemy = this.creatEnemy()
-
+    this.menu = this.createMenu()
+    
     this.anims.create({
       key: 'idle',
       frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 5 }),
@@ -76,7 +84,7 @@ class Background extends Phaser.Scene {
     // enemy animation --------------------
     this.anims.create({
       key: 'enemy_idle',
-      frames: this.anims.generateFrameNumbers('enemy', { start: 60, end: 68 }),
+      frames: this.anims.generateFrameNumbers('enemy', { start: 66, end: 67 }),
       frameRate: 8,
       repeat: -1
 
@@ -84,7 +92,7 @@ class Background extends Phaser.Scene {
 
     this.anims.create({
       key: 'enemy_run',
-      frames: this.anims.generateFrameNumbers('enemy', { start: 0, end: 8 }),
+      frames: this.anims.generateFrameNumbers('enemy', { start: 80, end: 88 }),
       FrameRate: 8,
       repeat: -1
 
@@ -184,10 +192,31 @@ class Background extends Phaser.Scene {
     enemy.setCollideWorldBounds(true);
 
     //enemy.body.setOffset(18, 13);
+    /////////////////////////////////////////////////////
+    //  // enemy weapon properties
+    //  enemyWeapon = game.add.weapon(5, 'enemy-bullet');
+    //  enemyWeapon.fireRate = 250;
+    //  enemyWeapon.bulletSpeed = 400;
+    //  enemyWeapon.bulletKillType = Phaser.Weapon.KILL_CAMERA_BOUNDS;
+
+    //  enemyWeapon.onFire.add(function() {
+    //      enemyFireSound.play();
+    //  });
+    enemy.body.velocity.x = 100;
+    // game.rnd.integerInRange(125, 175) * game.rnd.sign();
+    enemy.stepCount = 0
+    // game.rnd.integerInRange(0, stepLimit);
+
+    enemy.body.bounce.x = 1;
+    // platforms.setAll('body.immovable', true);
+    
+/////////////////////////////////////////////////////////////////////
+
 
     return enemy;
 
   }
+
   cameraFollow(player) {
     const { height, wdith, zoomFactor } = config;
     //this.physics.world.setBounds(0, 0, wdith + mapOffset, height)
@@ -203,6 +232,7 @@ class Background extends Phaser.Scene {
 
     }
   }
+
   endOfLevel(end, player) {
 
     //console.log(end.x, end.y, "end")
@@ -221,14 +251,144 @@ class Background extends Phaser.Scene {
 
   }
 
+  createMenu() {
+    center = {
+      x:this.physics.world.bounds.width / 2.5,
+      y:this.physics.world.bounds.height / 1.5
+    }
+  //creat a menu
+      // this.add.image(center.x , center.y  * 0.20, 'title')
+      //---------------------start
+      let startButton = this.add.image(center.x , center.y -150 , 'button')
+      startButton.setScale(1 , 2)
+      this.startText = this.add.text(center.x, center.y -150,  ' START ' , {
+          fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
+          fontSize: 30
+        });
+        this.startText.setOrigin(0.5, 0.5);
+      //-----------------------help
+      let helpButton = this.add.image(center.x , center.y , 'button')
+      helpButton.setScale(1 , 2)
+      this.helpText = this.add.text(center.x, center.y ,  ' HELP ' , {
+          fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
+          fontSize: 30
+        });
+        this.helpText.setOrigin(0.5, 0.5);
+      // ---------------------quit
+      let quitButton = this.add.image(center.x , center.y + 150 , 'button')
+      quitButton.setScale(1 , 2)
+      this.quitText = this.add.text(center.x, center.y + 150 ,  ' QUIT ' , {
+          fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
+          fontSize: 30
+        });
+        this.quitText.setOrigin(0.5, 0.5);
+
+      
+      // this.sound.play('title-music', {
+      //     loop :  true
+      // })
+
+      //----------------------help Box
+      let helpBox = this.add.image(center.x , center.y , 'helpBox')
+      helpBox.setScale(6 , 11)
+      helpBox.visible = false;
+      this.helpBoxText = this.add.text(center.x, center.y ,  ' helpBox ' , {
+          fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
+          fontSize: 30
+        });
+        this.helpBoxText.setOrigin(0.5, 0.5);
+        this.helpBoxText.visible = false;
+
+        let closeHelpBox = this.add.image(center.x , center.y  , 'closeHelpBox')
+        // closeHelpBox.setScale(1 , 2)
+        closeHelpBox.setOrigin(-2.2, 5.7);
+        closeHelpBox.visible = false;
+
+      
+
+      //-------------click on buttons-------------
+        //----------------start
+      startButton.setInteractive();
+      startButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER,()=> {
+          startButton.setScale (0.9 ,1.9)
+          this.startText.setScale(.8)
+      })
+      startButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT,()=> {
+          startButton.setScale(1 , 2)
+          this.startText.setScale(1)
+      })
+      
+      startButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN ,() => {
+          clickStart = true 
+          startButton.visible = false;
+          this.startText.visible = false;
+          helpButton.visible = false;
+          this.helpText.visible = false;
+          quitButton.visible = false;
+          this.quitText.visible = false;
+          })
+
+          //------------------help
+      helpButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER,()=> {
+          helpButton.setScale (0.9 ,1.9)
+          this.helpText.setScale(.8)
+      })
+      helpButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT,()=> {
+          helpButton.setScale(1 , 2)
+          this.helpText.setScale(1)
+      })
+
+      helpButton.setInteractive();
+      helpButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN ,() => {
+        helpBox.visible =true
+        this.helpBoxText.visible = true
+        closeHelpBox.visible = true
+      })
+
+      //------------------quit
+      quitButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER,()=> {
+          quitButton.setScale (0.9 ,1.9)
+          this.quitText.setScale(.8)
+      })
+      quitButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT,()=> {
+          quitButton.setScale(1 , 2)
+          this.quitText.setScale(1)
+      })
+      quitButton.setInteractive();
+      quitButton.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN ,() => {
+          window.close()
+      })
+
+
+      //------------------close of helpBox
+      closeHelpBox.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER,()=> {
+        closeHelpBox.setScale (0.99 , 0.99)
+      })
+      closeHelpBox.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT,()=> {
+        closeHelpBox.setScale(1 , 1)
+      })
+      closeHelpBox.setInteractive();
+      closeHelpBox.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN ,() => {
+        this.helpBoxText.visible = false;
+        closeHelpBox.visible = false;
+        helpBox.visible =false;
+      })
+
+  }
+
 
 
 
 
   update() {
 
-
-
+    // if (clickStart = false) { 
+    //   this.player.anims.play('idle', true);
+    //   this.enemy.anims.play('enemy_idle', true)
+    // }
+    // else 
+    if (clickStart)
+    { 
 
     const onFloor = this.player.body.onFloor(); // checks if the player has touched the platform collider 
     const cursors = this.input.keyboard.createCursorKeys();
@@ -343,7 +503,111 @@ class Background extends Phaser.Scene {
 
     if (this.player.body.blocked.down) dbJump = 0;
     //console.log(player.y);
+ 
+    // game.physics.arcade.collide(enemy, platforms);
+    // this.physics.add.collide(enemy, platforms);
+    // this.physics.add.overlap(player, enemy, touchEnemy, null, this);
+    // game.physics.arcade.overlap(player, enemy, touchEnemy, null, this);
+    // this.physics.add.collider(enemy, platforms, patrolPlatform, null, this);
+
+        
+    // console.log(Phaser.Math.Distance.Between(this.player.x, null, this.enemy.x, null))
+    // see if enemy and player within 400px of each other
+    if (Phaser.Math.Distance.Between(this.player.x, null, this.enemy.x, null) < 300 && Phaser.Math.Distance.Between(null, this.player.y, null, this.enemy.y) < 100) {
+
+        // if player to left of enemy AND enemy moving to right (or not moving)
+        if (this.enemy.body.velocity.x >= 0){               
+            //  enemy.anims.play('enemy-run-left')
+            if (this.player.x < this.enemy.x ) {
+                // move enemy to left
+                this.enemy.body.velocity.x = -150;
+        }}
+        // if player to right of enemy AND enemy moving to left (or not moving)
+        else if (this.enemy.body.velocity.x <= 0) {                 
+                // enemy.anims.play('enemy-run-right')
+                if (this.player.x > this.enemy.x){
+                    // move enemy to right
+                    this.enemy.body.velocity.x = 150;
+        }}
+    }
+           
+    // enemyGroup.forEachAlive(function (enemy) {
+        // if bottom positions equal (could be on same platform) AND player within 300px
+        if (this.player.y == this.enemy.y && Phaser.Math.Distance.Between(this.player.x, null, this.enemy.x, null) < 300) {    
+            // if player to left of enemy AND enemy moving to right
+            if (this.player.x < this.enemy.x && this.enemy.body.velocity.x > 0) {
+                // move enemy to left            
+                this.enemy.body.velocity.x *= -1; // reverse direction
+                // or could set directly: enemy.body.velocity.x = -150;        
+                // could add other code - change enemy animation, make enemy fire weapon, etc.
+                // enemyWeapon.fireAngle = -180;
+                // enemyWeapon.fire();
+            }
+            // if player to right of enemy AND enemy moving to left
+            else if (this.player.x > this.enemy.x && this.enemy.body.velocity.x < 0) {
+                // move enemy to right
+                this.enemy.body.velocity.x *= -1; // reverse direction
+                // or could set directly: enemy.body.velocity.x = 150;
+                // could add other code - change enemy animation, make enemy fire weapon, etc.
+                // enemyWeapon.fireAngle = 0;
+                // enemyWeapon.fire();
+            }
+        }
+    // });
+  
+  // enemyGroup.forEachAlive(function (enemy) {
+        // increase enemy's step counter
+        this.enemy.stepCount++;
+        // check if enemy's step counter has reach limit
+        if (this.enemy.stepCount > stepLimit) {
+            // reverse enemy direction
+            this.enemy.body.velocity.x *= -1;
+            // reset enemy's step counter
+            this.enemy.stepCount = 0;
+            // can add other code - change enemy animation, etc.
+        }
+      // });
+
+  // enemyWeapon.trackSprite(enemy); // give weapon to this enemy
+  // enemyWeapon.fireAngle = 0; // if necessary, change fire angle
+  // enemyWeapon.fire();
+
+
+  // this.enemy.enemyObject.setVelocityX(50*this.enemy.direction)
+
+  // enemy.setVelocityX (200);
+
+
+  if (this.enemy.body.velocity.x > 1){
+    this.enemy.anims.play('enemy_run', true)
+    this.enemy.flipX = true
+   //  this.enemy.anims.play('enemy-run-right', true)
+ }
+ else if(this.enemy.body.velocity.x < -1){
+    this.enemy.anims.play('enemy_run', true)
+    this.enemy.flipX = false
+   //  this.enemy.anims.play('enemy-run-left', true)
+ }
+ else {
+   this.enemy.anims.play('enemy_idle', true)
+  //  if (this.enemy.flipX){
+  //   this.enemy.flipX =true
+  //  this.enemy.anims.play('enemy_idle', true)
+  // }
+  // else {
+  //   this.enemy.anims.play('enemy_idle', true)
+  // }
+ }
+//......................
+      
+ 
+}
+else { 
+    this.player.anims.play('idle', true);
+    this.enemy.anims.play('enemy_idle', true)
+    this.enemy.body.velocity.x = 0;
   }
+}
 
 }
 
@@ -351,4 +615,10 @@ class Background extends Phaser.Scene {
 function render() {
   game.debug.inputInfo(32, 32);
 }
+
+// function loading()
+// {
+//   window.open (url);
+//  //  onload= "loading()"
+// }
 
